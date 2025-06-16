09ceded62036db4b11c9c9aef83d23001dc57e9f
miche <AhmedSA19980>
2025-06-16 17:53:58 +0300

    main file app.ts

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+//import { Data } from "./data/data";
+//import UserInfo from "./interface/user";
+//import CompanyInfo from "./interface/company";
+//import AddressInfo from "./interface/address";
+import { DisplayUserInfo } from "./components/display/displayuserinfo";
+
+
+const Users = new DisplayUserInfo();
+Users.DisplayUsers();
