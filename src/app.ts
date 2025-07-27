426111b6dcec7335b5a5a4a1d42cf16e15d64af9
miche <AhmedSA19980>
2025-07-28 01:22:38 +0300

    prepare to remove app file

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,5 @@
 //import UserInfo from "./interface/user";
 //import CompanyInfo from "./interface/company";
 //import AddressInfo from "./interface/address";
-import { DisplayUserInfo } from "./components/display/displayuserinfo";
 
 
-const Users = new DisplayUserInfo();
-Users.DisplayUsers();
