6ca2dcd6dee489c336995002a9f54c42d5a4fe8d
miche <AhmedSA19980>
2025-07-28 01:24:29 +0300

    remove app file

--- src/app.ts
+++
@@ -1,6 +0,0 @@
-//import { Data } from "./data/data";
-//import UserInfo from "./interface/user";
-//import CompanyInfo from "./interface/company";
-//import AddressInfo from "./interface/address";
-
-
